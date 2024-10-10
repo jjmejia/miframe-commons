4e140a5d46c489edb55e1bf8c1307829f6095262
John Mejia <john@smartinfo.com.co>
2024-10-10 15:42:15 -0500

    Complemento de demos a pantalla

--- src/miframe/commons/helpers.php
+++ src/miframe/commons/helpers.php
@@ -8,6 +8,7 @@
  */
 
 use miFrame\Commons\Core\ServerData;
+use miFrame\Commons\Core\ShowMe;
 
 /**
  * Retorna Clase para manejo de valores registrados en $_SERVER y  funcionalidades asociadas.
@@ -17,4 +18,24 @@ use miFrame\Commons\Core\ServerData;
 function miframe_server() : ServerData {
 
 	return ServerData::getInstance();
+}
+
+/**
+ * ShowMe
+ */
+function miframe_show() : ShowMe {
+
+	return ShowMe::getInstance();
+}
+
+/**
+ * Simplifica uso de ShowMe
+ */
+function miframe_box(string $body, string $title = '', string $footnote = '', string $class = '') {
+
+	return miframe_show()->title($title)
+		->body($body)
+		->footer($footnote)
+		->class($class)
+		->render(true);
 }
