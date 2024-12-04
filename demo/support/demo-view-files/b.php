7786490c19138089cd354c1dca802a03a33b8a37
John Mejia <john@smartinfo.com.co>
2024-12-04 10:59:34 -0500

    Update b.php

--- demo/support/demo-view-files/b.php
+++ demo/support/demo-view-files/b.php
@@ -2,5 +2,9 @@
 	<p>Fecha: <span><?= date('Y/m/d H:i:s') ?></span></p>
 	<p>Texto: <span><?= trim($dato1) ?></span></p>
 	<p>Número: <span><?= number_format($dato2) ?></span></p>
+	<p>
+		<i><b>Aviso:</b> El reporte de errores en pantalla puede que esté bloqueado en el servidor.
+		Si no se visualiza mensaje de error en pantalla, prueba a habilitar el "modo Desarrollo".</i>
+	</p>
 	<p>Valor no asignado: <span><?= trim($dato3) ?></span></p>
 </div>
