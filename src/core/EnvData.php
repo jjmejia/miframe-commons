33ee9b4a5858f5fd07761e4b6f3aa57f65fb9dbb
John Mejia <14930638+jjmejia@users.noreply.github.com>
2025-04-14 19:34:39 -0500

    Update EnvData.php
    Documenta y adiciona métodos para mejor soporte funcional.

--- src/core/EnvData.php
+++ src/core/EnvData.php
@@ -1,7 +1,10 @@
 <?php
 
 /**
- * Clase para manejo de datos capturados de archivos .env
+ * Clase para manejo de datos de entorno, capturados de archivos .env
+ *
+ * @author John Mejía
+ * @since Marzo 2025
  */
 
 namespace miFrame\Commons\Core;
@@ -10,9 +13,15 @@ use miFrame\Commons\Patterns\Singleton;
 
 class EnvData extends Singleton {
 
+	/**
+	 * @var array $data Arreglo que contiene los datos del entorno.
+	 */
 	private array $data = [];
-	private string $filename = '';
-	private string $prefix = '';
+
+	/**
+	 * @var array $basenames Arreglo con el listado de archivos cargados.
+	 */
+	private array $basenames = [];
 
 	protected function singletonStart()
 	{
@@ -20,10 +29,15 @@ class EnvData extends Singleton {
 	}
 
 	/**
-	 * $name es "case insensitive".
+	 * Recupera el valor de una variable de entorno.
+	 *
+	 * @param string $name El nombre de la variable de entorno a recuperar.
+	 * @param mixed $default El valor predeterminado a devolver si la variable de entorno no está configurada. Por defecto, es una cadena vacía.
+	 * @return mixed El valor de la variable de entorno o el valor predeterminado.
 	 */
 	public function get(string $name, mixed $default = ''): mixed
 	{
+		// $name es "case insensitive".
 		$name = strtolower(trim($name));
 		if ($name !== '' && array_key_exists($name, $this->data)) {
 			return $this->data[$name];
@@ -31,19 +45,28 @@ class EnvData extends Singleton {
 		return $default;
 	}
 
+	/**
+	 * Recupera el valor de una variable de entorno como número válido.
+	 *
+	 * @param string $name El nombre de la variable de entorno a recuperar.
+	 * @param int|float $default El valor predeterminado a devolver si la variable no está definida o no es numérica. Por defecto es 0.
+	 * @return int|float El valor numérico de la variable de entorno o el valor predeterminado.
+	 */
 	public function getNumber(string $name, int|float $default = 0): int|float
 	{
 		return $this->get($name, $default) + 0;
 	}
 
+	/**
+	 * Recupera el valor de una variable de entorno como true o false (boolean).
+	 *
+	 * @param string $name El nombre de la variable de entorno a recuperar.
+	 * @param int|float $default El valor predeterminado a devolver si la variable no está definida o no es numérica. Por defecto es false.
+	 * @return int|float true o false según la variable de entorno o el valor predeterminado.
+	 */
 	public function getBoolean(string $name, bool $default = false): bool
 	{
 		return boolval($this->get($name, $default));
-	}
-
-	public function url(string $name, string $default = ''): string
-	{
-		return miframe_server()->host($this->get($name, $default));
 	}
 
 	public function documentRoot(string $name, string $default = ''): string
@@ -51,16 +74,27 @@ class EnvData extends Singleton {
 		return miframe_server()->documentRoot($this->get($name, $default));
 	}
 
+	/**
+	 * Carga las variables de entorno.
+	 *
+	 * Por defecto busca el archivo .env en cada directorio del proyecto, empezando en el
+	 * más externo declarado en SCRIPT_NAME y buscando hacia atrás hasta el directorio raíz
+	 * (DOCUMENT_ROOT).
+	 *
+	 * Puede indicar un nombre base para el archivo, de forma que busca "[basename].env".
+	 *
+	 * @param string $prefix Nombre base para el archivo de variables.
+	 * @return bool Devuelve true si las variables de entorno se cargaron correctamente, false en caso contrario.
+	 */
 	public function load(string $prefix = ''): bool
 	{
 		$result = false;
 		$basename = trim($prefix) . '.env';
 
-		if ($this->filename === '' || $this->prefix !== $prefix)
+		if (!isset($this->basenames[$prefix]))
 		{
 			// Limpia datos previos (si alguno)
-			$this->prefix = $prefix;
-			$this->data = [];
+			// $this->data = [];
 			$server = miframe_server();
 			// Busca archivo
 			$path = $server->documentRoot($basename);
@@ -81,12 +115,75 @@ class EnvData extends Singleton {
 				$env_data = parse_ini_file($path);
 				// Convierte todas las llaves a minusculas
 				if (is_array($env_data) && count($env_data) > 0) {
-					$this->data = array_change_key_case($env_data, CASE_LOWER);
+					$this->save($env_data);
 				}
-				$this->filename = $path;
+				// Preserva path asociado al prefijo actual
+				$this->basenames[$prefix] = $path;
 			}
 		}
 
 		return $result;
+	}
+
+	/**
+	 * Establece un valor para una variable de entorno.
+	 *
+	 * @param string $name Nombre de la variable de entorno.
+	 * @param mixed $value Valor que se asignará a la variable.
+	 */
+	public function set(string $name, mixed $value)
+	{
+		$name = strtolower(trim($name));
+		if ($name !== '' && !is_numeric($name)) {
+			$this->data[$name] = $value;
+		}
+	}
+
+	/**
+	 * Establece valores para múltiples variables de entorno.
+	 *
+	 * @param array $data Datos a guardar en el entorno.
+	 */
+	public function save(array $data)
+	{
+		// Elimina llaves numéricas
+		// $this->data += array_change_key_case($env_data, CASE_LOWER);
+		foreach ($data as $key => $value) {
+			$this->set($key, $value);
+		}
+	}
+
+	/**
+	 * Elimina los variables de entorno previamente establecidos.
+	 */
+	public function reset()
+	{
+		$this->data = [];
+	}
+
+	/**
+	 * Recarga los datos del entorno desde su fuente original.
+	 *
+	 * Este método permite actualizar los valores almacenados en la instancia
+	 * con los datos más recientes disponibles en los archivos de declaraciones (.env).
+	 * Cualquier valor modificado previamente se perderá.
+	 */
+	public function reload()
+	{
+		$basenames = array_keys($this->basenames);
+		$this->reset();
+		foreach ($basenames as $prefix) {
+			$this->load($prefix);
+		}
+	}
+
+	/**
+	 * Devuelve una lista de los archivos .env cargados.
+	 *
+	 * @return array Lista de archivos .env cargados, con sus rutas completas.
+	 */
+	public function loadedFiles(): array
+	{
+		return $this->basenames;
 	}
 }
