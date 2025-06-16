ace4f56505e0f456032abe9705bb79e1a9f16987
John Mejia <14930638+jjmejia@users.noreply.github.com>
2025-06-16 16:19:10 -0500

    Update demo-check.php
    Limpia archivo y habilita presentación del footer

--- demo/support/demo-check.php
+++ demo/support/demo-check.php
@@ -16,7 +16,6 @@ $Test->title = 'timecheck()';
 $Test->description = 'Demostración uso de chequeo de tiempos usando <code>timecheck()</code>';
 $Test->start();
 
-
 // Starting code...
 timecheck('START');
 
@@ -45,53 +44,10 @@ bloque(100,150, 'Closing...');
 
 timecheck('END');
 
-// $rustart = getrusage();
-// print_r($rustart);
+// Cierre de la página
+$Test->end();
 
-/*
-function timecheck(string $text = '') {
-
-	$now = microtime(true);
-
-	// Check if was setting the starting time.
-	// Usually, $_SERVER['REQUEST_TIME_FLOAT'] is setting by the web server.
-	if (!isset($_SERVER['REQUEST_TIME_FLOAT'])) {
-		$_SERVER['REQUEST_TIME_FLOAT'] = $now;
-	}
-
-	// Check if was setting value for checking point
-	if (!isset($GLOBALS['TIMETRIAL_PARTIAL_TIME'])) {
-		$GLOBALS['TIMETRIAL_PARTIAL_TIME'] = $now;
-	}
-
-	// Get time in seconds and 2 decimals
-	$partial = number_format(($now - $GLOBALS['TIMETRIAL_PARTIAL_TIME']), 3);
-
-	// Update checking point
-	$GLOBALS['TIMETRIAL_PARTIAL_TIME'] = $now;
-
-	// Get script source
-	$trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
-
-	// Use tThe first element in the $trace array to get the script source
-	$source = str_replace($_SERVER['DOCUMENT_ROOT'] . '\\', '', $trace[0]['file']);
-
-	// Get time in seconds and 2 decimals
-	$time = number_format(($now - $_SERVER['REQUEST_TIME_FLOAT']), 3);
-
-	// Add label (optional)
-	if ($text != '') {
-		$text = "<div style=\"float:right;padding-left:5px;color:cyan\">{$text}</div>";
-	}
-
-	// Show message, add some styles
-	echo PHP_EOL . "<div style=\"font-family:Calibri;background:#000;color:#fefefe;padding:5px;margin:5px 0;font-size:14px\">" .
-		"<b style=\"color:yellow\">TIME/TRIAL</b> Ellapse time: <b>{$time}</b> / Since last check-point: <b>{$partial}</b>{$text}" .
-		"<div style=\"color:#ccc;font-size:12px;padding-top:3px\">{$source}:{$trace[0]['line']}</div>".
-		"</div>" . PHP_EOL;
-
-}
-*/
+// Funciones de soporte
 
 function bloque($min, $max, $text = '') {
 global $b;
